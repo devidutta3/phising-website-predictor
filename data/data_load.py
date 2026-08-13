c400da6f7c14e1d649c09477929486fef39aeeee
Devidutta Das <dasdevidutta3@gmail.com>
2026-08-13 13:43:25 +0530

    committed

---
+++ data/data_load.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def data(range_data):
+    df=pd.read_csv(r"data\raw_data.csv")
+    return df.head(range_data)
+
+if __name__=="__main__":
+    range_data=int(input("Enter The Number To Print First:"))
+    result = data(range_data)
+    print(result)
+    
