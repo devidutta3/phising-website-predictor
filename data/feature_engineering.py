5ea069b8cfc4792b86d47b28770e97f3ac5f5316
Devidutta Das <dasdevidutta3@gmail.com>
2026-08-29 00:20:28 +0530

    committed

--- data/feature_engineering.py
+++ data/feature_engineering.py
@@ -1,8 +1,13 @@
 import pandas as pd 
 def extract_features(df):
     data=pd.DataFrame()
-    data["url_length"]=data["url"].apply(len)
-
+    data["url_length"]=df["url"].apply(len)
+    data["num_dots"]=df["url"].apply(lambda x: x.count("."))
+    print(df[["url" , "label"]]
+          .head()
+          )
+    print(df["label"].value_counts())
     return data
-
-df=pd.read_csv(r"data\raw_data.csv")
+df=pd.read_csv(r"data\\raw_data.csv")
+features=extract_features(df)
+print(features.head())
